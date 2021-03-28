9c481a9827950a806fd87d238792e65781937d79
Hattomo <45988993+Hattomo@users.noreply.github.com>
2021-03-28 18:43:37 +0900

    Day 5e

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -1,3 +1,4 @@
+#include "console.hpp"
 #include "font.hpp"
 #include "frame_buffer_config.hpp"
 #include "graphics.hpp"
@@ -46,8 +47,13 @@ extern "C" void KernelMain(const FrameBufferConfig &frame_buffer_config) {
     }
     WriteString(*pixel_writer, 0, 66, "Hello world!!", {0, 0, 255});
     char buf[128];
-    sprintf(buf, "1+2=%d", 1 + 2);
-    WriteString(*pixel_writer, 0, 82, buf, {0, 0, 255});
+    // sprintf(buf, "1+2=%d", 1 + 2);
+    // WriteString(*pixel_writer, 0, 82, buf, {0, 0, 255});
+    Console console(*pixel_writer, {0, 0, 0}, {255, 255, 255});
+    for (int i = 0; i < 49; ++i) {
+        sprintf(buf, "1+2=%d\n", i);
+        console.PutString(buf);
+    }
     while (1) {
         __asm__("hlt");
     }
